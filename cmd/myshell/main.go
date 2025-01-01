80c2ca0c2a16a940eac4b50edf214e0d956d4f42
driptaroop <driptaroop.das@dkbcodefactory.com>
2025-01-01 22:43:46 +0100

    command not found

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -14,5 +15,25 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	command = strings.TrimSpace(command)
+
+	// create an array of commands
+	commands := []string{}
+
+	// check if string is present in an array
+	if contains(commands, command) {
+		fmt.Fprintf(os.Stdout, "Command found")
+	} else {
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
+	}
+}
+
+func contains(arr []string, str string) bool {
+	for _, s := range arr {
+		if s == str {
+			return true
+		}
+	}
+	return false
 }
