235a167dab1b3e1fc893db5dd751e9bdda4d5ec5
driptaroop <driptaroop.das@dkbcodefactory.com>
2025-01-03 17:58:26 +0100

    fix(main.go): change func(string) to func([]string) in commands and evaluateCommand to match new argument type feat(main.go): add support for process.env.PORT environment variable to be able to run app on a configurable port feat(main.go): update validateCommand, commandInput, and preprocessArguments functions to handle []string instead of string

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,7 +11,7 @@ import (
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
-var commands map[string]func(string)
+var commands map[string]func([]string)
 
 func main() {
 	commands = builtinDefinition()
@@ -32,17 +32,19 @@ func commandInput() {
 func validateCommand(command string) {
 	//get all except the first word in command
 	command, argument, _ := strings.Cut(command, " ")
-	argument = preprocessArguments(argument)
+	fmt.Printf("arg: %s\n", argument)
+	arguments := preprocessArguments(argument)
+	fmt.Printf("arg: %s\n", arguments)
 
 	if f, ok := commands[command]; ok {
-		evaluateCommand(f, argument)
+		evaluateCommand(f, arguments)
 	} else {
 		paths := strings.Split(os.Getenv("PATH"), ":")
 		for _, path := range paths {
 			dir, _ := os.ReadDir(path)
 			for _, file := range dir {
 				if file.Name() == command {
-					output, _ := exec.Command(command, argument).Output()
+					output, _ := exec.Command(command, arguments...).Output()
 					fmt.Fprintf(os.Stdout, "%s", output)
 					return
 				}
@@ -52,32 +54,32 @@ func validateCommand(command string) {
 	}
 }
 
-func evaluateCommand(f func(string), argument string) {
-	f(argument)
+func evaluateCommand(f func([]string), arguments []string) {
+	f(arguments)
 }
 
-func builtinDefinition() map[string]func(string) {
-	return map[string]func(string){
-		"exit": func(exitCode string) {
-			code, _ := strconv.Atoi(exitCode)
+func builtinDefinition() map[string]func([]string) {
+	return map[string]func([]string){
+		"exit": func(exitCode []string) {
+			code, _ := strconv.Atoi(exitCode[0])
 			os.Exit(code)
 		},
-		"echo": func(message string) {
-			fmt.Println(message)
+		"echo": func(message []string) {
+			fmt.Println(strings.Join(message, " "))
 		},
-		"pwd": func(_ string) {
+		"pwd": func(_ []string) {
 			dir, _ := os.Getwd()
 			fmt.Println(dir)
 		},
-		"type": func(command string) {
-			if _, ok := commands[command]; ok {
+		"type": func(command []string) {
+			if _, ok := commands[command[0]]; ok {
 				fmt.Printf("%s is a shell builtin\n", command)
 			} else {
 				paths := strings.Split(os.Getenv("PATH"), ":")
 				for _, path := range paths {
 					dir, _ := os.ReadDir(path)
 					for _, file := range dir {
-						if file.Name() == command {
+						if file.Name() == command[0] {
 							fmt.Printf("%s is %s/%s\n", command, path, command)
 							return
 						}
@@ -86,10 +88,9 @@ func builtinDefinition() map[string]func(string) {
 				fmt.Printf("%s: not found\n", command)
 			}
 		},
-		"cd": func(path string) {
+		"cd": func(path []string) {
 			currentUser, _ := os.UserHomeDir()
-			path = strings.ReplaceAll(path, "~", currentUser)
-			err := os.Chdir(path)
+			err := os.Chdir(strings.ReplaceAll(path[0], "~", currentUser))
 			if err != nil {
 				fmt.Printf("cd: %s: No such file or directory\n", path)
 			}
@@ -97,13 +98,14 @@ func builtinDefinition() map[string]func(string) {
 	}
 }
 
-func preprocessArguments(arg string) string {
-	startesWithSingleQuote := strings.HasPrefix(arg, "'")
+func preprocessArguments(arg string) []string {
+	startsWithSingleQuote := strings.HasPrefix(arg, "'")
 	endsWithSingleQuote := strings.HasSuffix(arg, "'")
-	if startesWithSingleQuote && endsWithSingleQuote {
-		return strings.Trim(arg, "'")
+	if startsWithSingleQuote && endsWithSingleQuote {
+		arg = strings.Trim(arg, "'")
+		return strings.Split(arg, "' '")
 	} else {
 		fields := strings.Fields(arg)
-		return strings.Join(fields, " ")
+		return fields
 	}
 }
