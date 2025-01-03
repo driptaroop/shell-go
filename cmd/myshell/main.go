3946d15bc0da5aea80624991336bc49a721afc86
driptaroop <driptaroop.das@dkbcodefactory.com>
2025-01-03 18:00:59 +0100

    fix(main.go): pass the first argument to fmt.Printf instead of the whole slice path

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -90,7 +90,7 @@ func builtinDefinition() map[string]func([]string) {
 			currentUser, _ := os.UserHomeDir()
 			err := os.Chdir(strings.ReplaceAll(path[0], "~", currentUser))
 			if err != nil {
-				fmt.Printf("cd: %s: No such file or directory\n", path)
+				fmt.Printf("cd: %s: No such file or directory\n", path[0])
 			}
 		},
 	}
