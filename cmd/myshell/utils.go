ea6eebc98ce2ceb2a62ddacdf7b4128c81b8c53e
driptaroop <driptaroop.das@dkbcodefactory.com>
2025-01-05 13:17:24 +0100

    double quote

--- cmd/myshell/utils.go
+++ cmd/myshell/utils.go
@@ -5,9 +5,14 @@ import "strings"
 func PreprocessArguments(arg string) []string {
 	startsWithSingleQuote := strings.HasPrefix(arg, "'")
 	endsWithSingleQuote := strings.HasSuffix(arg, "'")
+	startsWithDDoubleQuote := strings.HasPrefix(arg, "\"")
+	endsWithDoubleQuote := strings.HasSuffix(arg, "\"")
 	if startsWithSingleQuote && endsWithSingleQuote {
 		arg = strings.Trim(arg, "'")
 		return strings.Split(arg, "' '")
+	} else if startsWithDDoubleQuote && endsWithDoubleQuote {
+		arg = strings.Trim(arg, "\"")
+		return strings.Split(arg, "\" \"")
 	} else {
 		fields := strings.Fields(arg)
 		return fields
